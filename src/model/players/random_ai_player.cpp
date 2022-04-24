da81b40cad549a9c9b7aa481902f11c2f24f2b0d
Evteev Tikhon <evteev.t.d@gmail.com>
2022-04-24 21:47:23 +0300

    changed to c++20 standard

--- src/model/players/random_ai_player.cpp
+++ src/model/players/random_ai_player.cpp
@@ -1,12 +1,14 @@
 #include "random_ai_player.hpp"
 
-#include <algorithm>
+// #include <algorithm>
+#include <random>
 
 Point RandomAIPlayer::getMove(const IField* field, Symbol) const {
     // do smth genious;
     const IBoard* b = field->getBoard();
     std::vector<Point> p = b->getCells();
-    random_shuffle(p.begin(), p.end());
+
+    shuffle(p.begin(), p.end(), std::default_random_engine(0));
     for (auto i : p) {
         if (b->at(i) == Symbol::Empty) return i;
     }
